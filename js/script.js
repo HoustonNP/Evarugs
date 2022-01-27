51c3890f9689dce6c58cc49cc87ba2ab45d3f386
HoustonNP <92217081+HoustonNP@users.noreply.github.com>
2022-01-27 14:13:49 +0300

    main без адаптива

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 window.onload = function () {
 	jQuery("#user-city").text(ymaps.geolocation.city);
 	jQuery("#user-city2").text(ymaps.geolocation.city);
+	jQuery("#user-city3").text(ymaps.geolocation.city);
 }
