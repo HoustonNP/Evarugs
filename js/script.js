0eeab0fd169e62d17ce7be7e7ffe5013dc658fcc
HoustonNP <92217081+HoustonNP@users.noreply.github.com>
2022-02-26 16:14:19 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,13 +3,3 @@ window.onload = function () {
 	jQuery("#user-city2").text(ymaps.geolocation.city);
 	jQuery("#user-city3").text(ymaps.geolocation.city);
 };
-
-const swiper = new Swiper('.swiper', {
-	slidesPerView: 3,
-	loop: true,
-	speed: 400,
-	spaceBetween: 70,
-	centeredSlides: true,
-	slideToClickedSlide: true,
-	toggle: true,
-});
